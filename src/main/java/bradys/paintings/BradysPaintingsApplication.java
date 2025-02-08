a608dd641132a6832870450a4ae163d29e127758
Brady Burgener <bradylbsee@gmail.com>
2025-02-08 13:13:04 -0700

    init

---
+++ src/main/java/bradys/paintings/BradysPaintingsApplication.java
@@ -0,0 +1,14 @@
+package bradys.paintings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradysPaintingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradysPaintingsApplication.class, args);
+
+	}
+
+}
